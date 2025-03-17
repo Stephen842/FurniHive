54d7fb8c77f6cdea202dc9aefcb5bbc5d69dd8cc
Stephen842 <ugotachisomstephen@gmail.com>
2025-03-17 14:59:26 +0100

    Worked on the product detailed page , did some hotfixing to the UI and also on views and urls, added some pages and currently focusing on the product category page

--- interior/furniture/urls.py
+++ interior/furniture/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.Store, name='store'),
     path('category/<slug:category_slug>/<slug:product_slug>/', views.Product_detail, name='product-detail'),
     path('category/<slug:category_slug>/', views.Category_products, name='category_products'),
+    path('search/', views.search, name='search'),
 ]
--- interior/furniture/views.py
+++ interior/furniture/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Q, Count
 from .models import Product, Category
 
 # Create your views here.
@@ -19,19 +20,33 @@ def Store(request):
 def Product_detail(request, category_slug, product_slug):
     product = get_object_or_404(Product, slug=product_slug, category__slug=category_slug)
     category = get_object_or_404(Category, slug=category_slug)
+    related = Product.objects.filter(category__in=product.category.all()).exclude(id=product.id).annotate(matching_categories=Count('category', filter=Q(category__in=product.category.all()))).filter(matching_categories__gt=0).distinct()
 
     context = {
         'category': category,
         'product': product,
+        'related': related,
     }    
     return render(request, 'pages/product_detail.html', context)
 
 def Category_products(request, category_slug):
-    category = get_object_or_404(Category, slug=category_slug)
+    categories = get_object_or_404(Category, slug=category_slug)
     products = Product.objects.filter(category=category)
 
     context = {
-        'category': category,
+        'category': categories,
         'products': products,
     }
     return render(request, 'pages/category_products.html', context)
+
+def search(request):
+    query = request.GET.get('q')
+    products = Product.objects.filter(Q(name__icontains=query) | Q(category__name__icontains=query))
+    categories = Category.objects.filter(name__icontains=query)
+    
+    context = {
+        'query': query,
+        'products': products,
+        'categories': categories,
+    }
+    return render(request, 'pages/search_results.html', context)
